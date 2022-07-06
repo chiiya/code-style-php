bf8b070b18545a612d042c7cfa37742ccc79cdfd
Elisha Witte <elisha.witte@antwerpes.de>
2022-07-06 11:20:08 +0200

    Update dependencies

--- src/ecs.php
+++ src/ecs.php
@@ -7,38 +7,43 @@ use SlevomatCodingStandard\Sniffs\ControlStructures\RequireMultiLineTernaryOpera
 use SlevomatCodingStandard\Sniffs\ControlStructures\RequireNullSafeObjectOperatorSniff;
 use SlevomatCodingStandard\Sniffs\PHP\DisallowDirectMagicInvokeCallSniff;
 use SlevomatCodingStandard\Sniffs\Variables\UselessVariableSniff;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\ArrayNotation\StandaloneLineInMultilineArrayFixer;
 use Symplify\CodingStandard\Fixer\Commenting\ParamReturnAndVarTagMalformsFixer;
 use Symplify\CodingStandard\Fixer\Commenting\RemoveUselessDefaultCommentFixer;
 use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $services
+return static function (ECSConfig $config): void {
+    // Ternary operator has to be reformatted to more lines when the line length exceeds the given limit.
+    $config->ruleWithConfiguration(RequireMultiLineTernaryOperatorSniff::class, [
+        'minExpressionsLength' => 60,
+    ]);
+    // Checks that there is a certain number of blank lines between methods.
+    $config->ruleWithConfiguration(MethodSpacingSniff::class, [
+        'minLinesCount' => 1,
+        'maxLinesCount' => 1,
+    ]);
+    $config->rules([
         // -----------------------
         // Simplify Coding Standard
         // -----------------------
         // Indexed arrays must have 1 item per line.
-        ->set(StandaloneLineInMultilineArrayFixer::class)
+        StandaloneLineInMultilineArrayFixer::class,
         // Fixes @param, @return, @var and inline @var annotations broken formats.
-        ->set(ParamReturnAndVarTagMalformsFixer::class)
+        ParamReturnAndVarTagMalformsFixer::class,
         // Remove useless PHPStorm-generated to do comments, redundant "Class XY" or "gets service" comments etc.
-        ->set(RemoveUselessDefaultCommentFixer::class)
+        RemoveUselessDefaultCommentFixer::class,
         // Array items, method parameters, method call arguments, new arguments should be on same/standalone line to fit line length.
-        ->set(LineLengthFixer::class)
+        LineLengthFixer::class,
 
         // -----------------------
         // Slevomat Coding Standard
         // -----------------------
         // Disallow direct calls of __invoke().
-        ->set(DisallowDirectMagicInvokeCallSniff::class)
+        DisallowDirectMagicInvokeCallSniff::class,
         // Requires using ?-> operator.
-        ->set(RequireNullSafeObjectOperatorSniff::class)
-        // Ternary operator has to be reformatted to more lines when the line length exceeds the given limit.
-        ->set(RequireMultiLineTernaryOperatorSniff::class)->property('minExpressionsLength', 60)
-        // Checks that there is a certain number of blank lines between methods.
-        ->set(MethodSpacingSniff::class)->property('minLinesCount', 1)->property('maxLinesCount', 1)
+        RequireNullSafeObjectOperatorSniff::class,
         // Remove useless variables.
-        ->set(UselessVariableSniff::class);
+        UselessVariableSniff::class,
+    ]);
 };
